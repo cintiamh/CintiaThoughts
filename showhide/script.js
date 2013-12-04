e2d8730f80bd3001d017c621bb3d8278920e23f8
Cintia Higashi <cintiamh@gmail.com>
2013-12-04 00:43:26 -0800

    code refactoring

--- showhide/script.js
+++ showhide/script.js
@@ -1,22 +1,28 @@
 window.onload=function() {
 	var list = document.getElementById('list');
 	for (var i = 0; i < list.childNodes.length; i++) {
-		if (list.childNodes[i].tagName === "LI") {
-			var list_item = list.childNodes[i];
-			for (var j = 0; j < list_item.childNodes.length; j++) {
-				if (list_item.childNodes[j].tagName === "A") {
-					link = list_item.childNodes[j];
-					link.addEventListener('click', function(event) {
-						event.preventDefault();
-						if (this.nextElementSibling.style.display === 'none') {
-							this.nextElementSibling.style.display = 'block';
-						}
-						else {
-							this.nextElementSibling.style.display = 'none';
-						}
-					});
-				}
-			}
-		}
+		iterate_list_item(list.childNodes[i]);
 	}
 };
+
+function iterate_list_item(list_item) {
+	if (list_item.tagName === "LI") {
+		for (var j = 0; j < list_item.childNodes.length; j++) {
+			iterate_details(list_item.childNodes[j]);
+		}
+	}
+}
+
+function iterate_details(link) {
+	if (link.tagName === "A") {
+		link.addEventListener('click', function(event) {
+			event.preventDefault();
+			if (this.nextElementSibling.style.display === 'none') {
+				this.nextElementSibling.style.display = 'block';
+			}
+			else {
+				this.nextElementSibling.style.display = 'none';
+			}
+		});
+	}
+}
